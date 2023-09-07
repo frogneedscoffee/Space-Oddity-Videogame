66ad2505fe2602c636dc29693faaccaefba6994e
Aguss <111596201+Agusschajris@users.noreply.github.com>
2023-09-07 10:51:32 -0300

    Subo códigos casi definitivos de arduino
    Estos son realmente los códigos de arduino que sirven, lo único que falta es agregarle las funciones del motor que Ivo encontró.

---
+++ Actually definitive arduino codes/DEFINES_LIBRERIAS_VARIABLES/DEFINES_LIBRER_AS_VARIABLES/DEFINES_LIBRER_AS_VARIABLES.ino
@@ -0,0 +1,43 @@
+//--------LIBRERÍAS--------
+#include <avr/wdt.h> //libreria del watchdog
+#include "Adafruit_GFX.h"// Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
+#include <Adafruit_TFTLCD.h>
+//Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+//--------COLORES--------
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+#define VIOLET 0xB527E1
+
+//--------PINES--------
+//TFT
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+//ULTRASONIDOS
+#define EchoPin 53
+#define TriggerPin 51
+
+//LED
+#define pinR 49
+#define pinG 47
+#define pinB 45
+
+//MOTOR
+
+
+//--------VARIABLES--------
+String currentPage;
+String Lectura = "";
+int numVida = 7;
---
+++ Actually definitive arduino codes/FUNCIONES/FUNCIONES/FUNCIONES.ino
@@ -0,0 +1,157 @@
+//--------FUNCIONES--------
+void lecturaSerie(){
+  if(Serial.available() > 0){
+    Lectura = Serial.readStringUntil('\n');
+    Lectura = Lectura.substring(0, Lectura.length());
+    Serial.println(Lectura);
+  }
+}
+
+void reset(){
+  wdt_enable(WDTO_15MS);
+  while(1){};
+}
+
+void drawHomeScreen (){
+  tft.fillScreen(BLACK);
+  
+  //TÍTULO
+  tft.setTextColor(MAGENTA);
+  tft.setTextSize(11);
+  tft.setCursor(80, 60);
+  tft.print("SPACE");
+  tft.setCursor(50, 160);
+  tft.print("ODDITY");
+  delay(250);
+
+  //STARS
+  tft.fillRect(20, 100, 7, 7, YELLOW);
+  tft.fillRect(400, 100, 7, 7, WHITE);
+  tft.fillRect(200, 30, 7, 7, WHITE);
+  tft.fillRect(150, 300, 7, 7, YELLOW);
+  tft.fillRect(80, 30, 7, 7, WHITE);
+  tft.fillRect(450, 30, 7, 7, WHITE);
+  tft.fillRect(20, 240, 7, 7, WHITE);
+  tft.fillRect(35, 300, 7, 7, WHITE);
+  tft.fillRect(250, 210, 7, 7, WHITE);
+  tft.fillRect(450, 210, 7, 7, YELLOW);
+  tft.fillRect(390, 290, 7, 7, WHITE);
+  tft.fillRect(450, 300, 7, 7, WHITE);
+  tft.fillRect(300, 280, 7, 7, WHITE);
+}
+
+void drawInGameScreen(){
+  tft.fillScreen(BLACK);
+
+  //STARS
+  tft.fillRect(20, 20, 7, 7, YELLOW);
+  tft.fillRect(200, 15, 7, 7, WHITE);
+  tft.fillRect(400, 100, 7, 7, WHITE);
+  tft.fillRect(200, 30, 7, 7, WHITE);
+  tft.fillRect(150, 300, 7, 7, YELLOW);
+  tft.fillRect(80, 30, 7, 7, WHITE);
+  tft.fillRect(450, 30, 7, 7, WHITE);
+  tft.fillRect(20, 240, 7, 7, WHITE);
+  tft.fillRect(35, 300, 7, 7, WHITE);
+  tft.fillRect(250, 210, 7, 7, WHITE);
+  tft.fillRect(450, 210, 7, 7, YELLOW);
+  tft.fillRect(390, 290, 7, 7, WHITE);
+  tft.fillRect(450, 300, 7, 7, WHITE);
+  tft.fillRect(300, 280, 7, 7, WHITE);
+
+  //RECTÁNGULO DE FONDO
+  tft.fillRoundRect(30, 30, 420, 260, 30, MAGENTA);
+
+  //VIDAS
+  tft.setTextColor(BLACK);
+  tft.setTextSize(5);
+  tft.setCursor(118, 200);
+  tft.print("Vidas: ");
+
+  tft.setTextColor(BLACK);
+  tft.setTextSize(5);
+  tft.setCursor(295, 200);
+  tft.print("x");
+
+  tft.setTextColor(BLACK);
+  tft.setTextSize(5);
+  tft.setCursor(330, 200);
+  tft.print(numVida);
+
+  //PLAY
+  tft.setTextColor(GREEN);
+  tft.setTextSize(12);
+  tft.setCursor(80, 80);
+  tft.print("PLAY!");
+}
+
+void drawDamageScreen(){
+  tft.fillScreen(BLACK);
+
+  for (int i = 0; i < 2; i++){
+    //PARPADEO DAMAGE
+    analogWrite(pinG, 255);
+    analogWrite(pinR, 0);
+    analogWrite(pinB, 255);
+
+    //FALTA MOTOR IVOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+  
+    tft.fillRect(0, 100, 480, 100, RED);
+    tft.setTextColor(WHITE);
+    tft.setTextSize(8);
+    tft.setCursor(95, 120);
+    tft.print("DAMAGE");
+  
+    delay(500);
+      
+    tft.fillScreen(BLACK);
+  
+    analogWrite(pinG, 255);
+    analogWrite(pinR, 255);
+    analogWrite(pinB, 255);
+    
+    delay(500);
+  }
+}
+
+void drawGameOverScreen(){
+  tft.fillScreen(BLACK);
+
+  //FALTA MOTOR IVOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+  
+  for (int i = 0; i < 2; i++){
+    analogWrite(pinG, 255);
+    analogWrite(pinR, 0);
+    analogWrite(pinB, 255);
+    
+    tft.fillRect(0, 100, 480, 100, RED);
+    tft.setTextColor(WHITE);
+    tft.setTextSize(7);
+    tft.setCursor(45, 120);
+    tft.print("Game Over!");
+    
+    delay(500);
+    
+    tft.fillScreen(BLACK);
+
+    analogWrite(pinG, 255);
+    analogWrite(pinR, 255);
+    analogWrite(pinB, 255);
+    
+    delay(500);
+  }
+
+  reset();
+}
+
+void drawVictoryScreen(){
+  tft.fillScreen(BLACK);
+  tft.fillRect(0, 100, 480, 100, GREEN);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(7);
+  tft.setCursor(70, 120);
+  tft.print("VICTORY");
+  delay(2000);
+
+  reset();
+}
